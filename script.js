af7b6b80b1036cbaf693601043c35ee314caa697
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-08-02 14:53:03 -0300

    adiciona requisito 2 e 3

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ const h1 = document.createElement('h1');
 h1.id = 'title';
 h1.innerText = 'Paleta de Cores';
 document.body.appendChild(h1);
+
+const main = document.createElement('main');
+document.body.appendChild(main);
+
+const div = document.createElement('div');
+div.id = 'color-palette';
+main.appendChild(div);
+
+const colors = ['black', 'blue', 'green', 'red'];
+for (let index = 0; index <= colors.length - 1; index += 1) {
+  const newDiv = document.createElement('div');
+  newDiv.className = 'color';
+  newDiv.style.background = colors[index];
+  div.appendChild(newDiv);
+}
